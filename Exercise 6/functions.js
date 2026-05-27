6e857a0d660869cd333d4c61542720c74470e4ac
stephanbennett42-ctrl <stephanbennett42@gmail.com>
2026-05-27 14:51:46 +0200

    Add Exercise 6 submission

---
+++ Exercise 6/functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return "Hello" + name + "!";
+}
+
+function add(num1, num2) {
+  return num1 = num2;
+}
+let greetingMessage = greet("Alice");
+let sumResult = add(15, 25);
+
+console.log(greetingMessage);
+console.log("The sum is: ", sumResult);
+
